cfeab1d291c3abffa8cdab6cd3d67d7ac5727e50
root <root@localhost>
2023-07-25 19:24:31 +0000

    pm2 configs

--- frontend/pm2.config.js
+++ frontend/pm2.config.js
@@ -3,7 +3,7 @@ module.exports = {
       {
         name: 'gb-frontend-3001', // Nombre de la aplicación
         script: 'node_modules/next/dist/bin/next', // Ruta del binario de Next.js
-        args: 'start 3001', // Argumentos para el comando "start" de Next.js
+        args: ['start', '-p', '3001'], // Argumentos para el comando "start" de Next.js
         instances: 'max', // Número de instancias (recomendado 'max' para utilizar todos los núcleos de CPU)
         autorestart: true, // Reiniciar automáticamente la aplicación en caso de fallo
         watch: false, // Desactivar el modo watch (Next.js se encargará del hot-reloading)
